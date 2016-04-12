c996c6e50b52351dfda57eb859d29355f6416113
Nathan Kleekamp <nkleekamp@gmail.com>
2016-04-12 12:17:02 -0400

    added screenshot, fixed theme metadata

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'corona' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'corona' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'corona' ), 'corona', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'corona' ), 'corona', '<a href="https://github.com/IIP-Design/corona" rel="designer">Office of Design</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->

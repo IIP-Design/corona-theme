7cd1c74b6ea06fbd0aeba6e1e9497b3b11298a21
Nathan Kleekamp <nkleekamp@gmail.com>
2016-04-25 09:14:29 -0400

    removed fancy nav css; found simpler implementation

--- page-templates/page_Front-style-one.php
+++ page-templates/page_Front-style-one.php
@@ -16,7 +16,7 @@
   get_header(); ?>
 
   <div class="cta-container">
-    <img class="cta-image" src="http://ylai.dev/wp-content/uploads/2016/04/banner2.jpg" alt="President Obama shaking hands with honorees" />
+    <img class="cta-image" src="https://ylai.edit.america.gov/wp-content/uploads/sites/2/2016/04/banner2-1.jpg" alt="President Obama shaking hands with honorees" />
     <div class="cta-gradient"></div>
     <div class="cta-text">
       <p>Develop as a leader, connect with peers across the region, and be the change you're looking for</p>

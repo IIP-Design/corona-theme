12d61a56bed44feb56c09213c3ffa58807ef91d1
Nathan Kleekamp <nkleekamp@gmail.com>
2016-04-25 14:27:37 -0400

    added output to template for call to action

--- page-templates/page_Front-style-one.php
+++ page-templates/page_Front-style-one.php
@@ -13,16 +13,11 @@
   * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/page-templates/
   */
 
-  get_header(); ?>
+  get_header();
 
-  <div class="cta-container">
-    <img class="cta-image" src="https://ylai.edit.america.gov/wp-content/uploads/sites/2/2016/04/banner2-1.jpg" alt="President Obama shaking hands with honorees" />
-    <div class="cta-gradient"></div>
-    <div class="cta-text">
-      <p>Develop as a leader, connect with peers across the region, and be the change you're looking for</p>
-      <a href="/the-fellowship/" class="button cta-button">apply now</a>
-    </div>
-	</div>
+  if ( class_exists('acf') && get_field('cta_markup') ) {
+    the_field('cta_markup');
+  } ?>
 
   <div class="content-sidebar-wrap">
 		<main id="main" class="content" role="main">

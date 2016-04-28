f44305ad0df6f2bfa44e5194e2f0b6f9d9f7f65f
Nathan Kleekamp <nkleekamp@gmail.com>
2016-04-28 14:38:46 -0400

    added documentation to the CSS, hooks, removed docs dir to be included in documentation site, small css refactoring as well, esp rlt.css

--- inc/hooks.php
+++ inc/hooks.php
@@ -8,13 +8,31 @@
   * @subpackage corona
   */
 
-// See 'header.php'
+
+/**
+  * Google Tag Manager - Allows for the easy insertion of the GTM code
+  *
+  * @since Corona 1.0
+  * @package WordPress
+  * @subpackage corona
+  * @example https://github.com/USStateDept/ylai-theme/blob/master/functions.php#L24
+  * @see header.php
+  */
+
 function google_tag_manager() {
   do_action( 'google_tag_manager' );
 }
 
 
-// See 'inc/template-tags.php'
+/**
+  * Posted on - Change or remove the byline, a.k.a. "Posted on"
+  *
+  * @since Corona 1.1
+  * @package WordPress
+  * @subpackage corona
+  * @see inc/template-tags.php
+  */
+
 function corona_posted_on() {
   do_action( 'corona_posted_on' );
 }

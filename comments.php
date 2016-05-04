ce04d78646032de101fc12534368dfa3c9302a25
Nathan Kleekamp <nkleekamp@gmail.com>
2016-05-04 11:56:52 -0400

    fixed markup to reflect documentation

--- comments.php
+++ comments.php
@@ -20,8 +20,7 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
-
+<div id="comments" class="entry-comments">
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>

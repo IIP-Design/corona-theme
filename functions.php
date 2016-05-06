580c0d63cff800f01b75258163f23f1ef55af53d
Nathan Kleekamp <nkleekamp@gmail.com>
2016-05-06 15:15:29 -0400

    bumped version number to 1.5.0, added version to enqueued scripts and styles

--- functions.php
+++ functions.php
@@ -214,8 +214,8 @@ add_filter( 'widget_text', 'do_shortcode' );
  * Enqueue scripts and styles.
  */
 function corona_scripts() {
-	wp_enqueue_style( 'corona-style', get_stylesheet_uri() );
-	wp_enqueue_script( 'corona-js', get_template_directory_uri() . '/js/dist/script.js', array(), '', true );
+	wp_enqueue_style( 'corona-style', get_stylesheet_uri(), array(), '1.5.0', 'all' );
+	wp_enqueue_script( 'corona-js', get_template_directory_uri() . '/js/dist/script.js', array(), '1.5.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );

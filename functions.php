24f756fe01c5ab56a45da0a566c64bfadabf8d68
Nathan Kleekamp <nkleekamp@gmail.com>
2016-05-11 10:05:21 -0400

    better custom header image support for domain mapping

--- functions.php
+++ functions.php
@@ -8,15 +8,3 @@
   */
 
 require_once( dirname( __FILE__ ) . '/lib/init.php' );
-
-
-
-/**
-  * Replace Customizer header image with a relative path since domain mapper doesn't map it to production
-  */
-function relative_header_image() {
-	$url = get_header_image();
-	$parsed = parse_url( $url );
-	$path = $parsed['path'];
-	echo esc_url( $path );
-}

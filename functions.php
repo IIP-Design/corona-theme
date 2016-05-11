f7163f876c8d1cec1ae2bdd39297bfe3512d037e
Nathan Kleekamp <nkleekamp@gmail.com>
2016-05-11 13:15:07 -0400

    added versioning to corona

--- functions.php
+++ functions.php
@@ -7,16 +7,36 @@
 	* @since 2.0
   */
 
-require_once( dirname( __FILE__ ) . '/lib/init.php' );
 
 
 
 /**
-  * Replace Customizer header image with a relative path since domain mapper doesn't map it to production
-  */
-function relative_header_image() {
-	$url = get_header_image();
-	$parsed = parse_url( $url );
-	$path = $parsed['path'];
-	echo esc_url( $path );
+	* Get the theme version from version.json
+	* Has to happen before Corona is initialized
+	*
+	* @package Wordpress
+	* @subpackage corona
+	* @since Corona 2.0
+	*/
+
+function corona_get_theme_version() {
+	$contents = file_get_contents( get_template_directory() . '/version.json' );
+
+	$json = json_decode($contents, true);
+	$version = $json['version'];
+
+	// remove the extra quote around the version number required by sass
+	$version = substr($version, 1, -1);
+
+	return $version;
 }
+
+add_action( 'corona_pre_init', 'corona_get_theme_version', 1, 0 );
+
+
+
+
+/**
+	* Initialize Corona
+	*/
+require_once( dirname( __FILE__ ) . '/lib/init.php' );
--- lib/inc/enqueue-styles.php
+++ lib/inc/enqueue-styles.php
@@ -2,14 +2,14 @@
 
 /**
   * Enqueue theme CSS files
-  * 
+  *
   * @package Wordpress
   * @subpackage corona
   * @since Corona 2.0
   */
 
 function corona_styles() {
-	wp_enqueue_style( 'corona-style', get_stylesheet_uri(), array(), THEME_VERSION, 'all' );
+	wp_enqueue_style( 'corona-style', get_stylesheet_uri(), array(), CORONA_THEME_VERSION, 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'corona_styles' );

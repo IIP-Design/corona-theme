d753a80881f7a7824c76ce16c354c9a47d7203fc
Nathan Kleekamp <nkleekamp@gmail.com>
2016-05-17 16:44:08 -0400

    removed america flexible sidebar related code

--- sidebar.php
+++ sidebar.php
@@ -13,31 +13,6 @@
 	<?php tha_sidebars_before(); ?>
 
 	<?php
-	// @todo: This should be expanded to look more like the advanced loop option
-	// on this page: https://www.advancedcustomfields.com/resources/repeater/
-
-	// Check if the Flexible Sidebar flexible content field has rows of data
-	if ( class_exists('acf') && have_rows('america_sidebar') ) { ?>
-		<aside class="sidebar sidebar-primary">
-			<?php tha_sidebar_top(); ?>
-
-			<?php while ( have_rows('america_sidebar') ) : the_row(); ?>
-
-				<section class="flexible-sidebar">
-					<?php the_sub_field('america_markup'); ?>
-				</section>
-
-			<?php endwhile; ?>
-
-			<?php tha_sidebar_bottom(); ?>
-		</aside>
-	</div><!-- .content-sidebar-wrap -->
-
-
-	<?php
-	// If it doesn't, show the global sidebars that have widgets assigned to them
-	} else {
-
 		// only show if widgets are assigned to it
 		if( is_active_sidebar( 'sidebar-primary') ) : ?>
 
@@ -60,7 +35,6 @@
 				<?php corona_sidebar_secondary_bottom(); ?>
 			</aside>
 
-		<?php endif; // end secondary sidebar
-	} // end check for america_sidebar ?>
+		<?php endif; // end secondary sidebar ?>
 
 	<?php tha_sidebars_after(); ?>

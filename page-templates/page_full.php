67d6fdf18ac7d805a1b088bb534474501ebb9411
Nathan Kleekamp <nkleekamp@gmail.com>
2016-05-23 07:55:00 -0400

    removed call-to-action specific code

--- page-templates/page_full.php
+++ page-templates/page_full.php
@@ -13,23 +13,37 @@
   * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/page-templates/
   */
 
-  get_header(); ?>
-  <main id="main" class="content full-width" role="main">
+  get_header();
+  tha_content_before();
+?>
 
-    <?php
-    while ( have_posts() ) : the_post();
+  <div class="content-sidebar-wrap">
+    <main id="main" class="content full-width" role="main">
 
-      get_template_part( 'template-parts/content', 'page' );
+      <?php
+        tha_content_top();
+        tha_content_while_before();
 
-      // If comments are open or we have at least one comment, load up the comment template.
-      if ( comments_open() || get_comments_number() ) :
-        comments_template();
-      endif;
+        while ( have_posts() ) : the_post();
 
-    endwhile; // End of the loop.
-    ?>
+          get_template_part( 'template-parts/content', 'page' );
 
-  </main><!-- #main -->
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
+          endif;
 
+        endwhile; // End of the loop.
+
+        tha_content_while_after();
+        tha_content_bottom();
+      ?>
+
+    </main><!-- #main -->
+  </div>
+
+  
   <?php
-  get_footer();
+    tha_content_after();
+    get_footer();
+  ?>

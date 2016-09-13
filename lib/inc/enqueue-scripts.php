e3025d51056879b024de0dde8c97b841978bb734
Nathan Kleekamp <nkleekamp@gmail.com>
2016-09-13 12:31:18 -0400

    added modernizr with html5shiv, html5printshiv, and flexbox detect

--- lib/inc/enqueue-scripts.php
+++ lib/inc/enqueue-scripts.php
@@ -10,6 +10,7 @@
 
 function corona_scripts() {
 	wp_enqueue_script( 'corona-js', get_template_directory_uri() . '/js/dist/script.js', array(), CORONA_THEME_VERSION, true );
+  wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/dist/modernizr-3.3.1.min.js', array(), '3.3.1', false);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );

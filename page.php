73a7c0bb05d9d3a342f5e5091dda1c97ba6fc2d0
Nathan Kleekamp <nkleekamp@gmail.com>
2016-09-20 13:26:43 -0400

    wrapped before/after entries in divs, made page-template/page_full.php follow other corona conventions

--- page.php
+++ page.php
@@ -19,7 +19,12 @@ get_header(); ?>
 		<?php tha_content_before(); ?>
 		<main id="main" class="content" role="main"><!-- post loop -->
 		<?php tha_content_top(); ?>
-		<?php dynamic_sidebar( 'before-entry' ); ?>
+
+    <?php if ( is_active_sidebar( 'before-entry' ) ) : ?>
+      <div class="before-entry">
+		    <?php dynamic_sidebar( 'before-entry' ); ?>
+      </div>
+    <?php endif; ?>
 
 			<?php
 			tha_content_while_before();
@@ -29,7 +34,12 @@ get_header(); ?>
 			tha_content_while_after();
 			?>
 
-		<?php dynamic_sidebar( 'after-entry' ); ?>
+    <?php if ( is_active_sidebar( 'after-entry' ) ) : ?>
+      <div class="after-entry">
+		    <?php dynamic_sidebar( 'after-entry' ); ?>
+      </div>
+    <?php endif; ?>
+
 		<?php tha_content_bottom(); ?>
 		</main><!-- #main -->
 		<?php tha_content_after(); ?>
